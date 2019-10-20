519fdbec30d7e042d0aa4662d82bee8c43410d69
glukicov <g.lukicov@ucl.ac.uk>
2019-10-20 00:32:21 -0500

    Week2 in progress

--- intro-to-dl/keras_utils_hse.py
+++ intro-to-dl/keras_utils_hse.py
@@ -3,7 +3,6 @@
 from collections import defaultdict
 import numpy as np
 from keras.models import save_model
-# import tensorflow as tf
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 import keras
